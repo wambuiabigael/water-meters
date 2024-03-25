c66df641d3c7a59f9d5c802a0e10934187526236
wambuiabigael <133865117+wambuiabigael@users.noreply.github.com>
2024-03-25 13:55:51 +0300

    Add files via upload

---
+++ htmlfiles/data/ACKWATERBOREHOLE_2.js
@@ -0,0 +1,8 @@
+var json_ACKWATERBOREHOLE_2 = {
+"type": "FeatureCollection",
+"name": "ACKWATERBOREHOLE_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "BOREHOLE N": "ACK Water Borehole", "BOREHOLE G": "-1.2643267 36.9657095 1553.5 4.5", "LATITUDE_": -1.2643267, "LONGITUDE_": 36.9657095, "ALTITUDE_": 1553.5, "PRECISION_": 4.5, "BOREHOLE D": "200 METRES", "BOREHOLE C": "PVC CASING", "MAINTENANC": "LAST MAINTENANCE PERFORMED ON 01\/01\/2024, REPLACING WORN-OUT PARTS AND CHECKING FOR LEAKS" }, "geometry": { "type": "Point", "coordinates": [ 36.9657095, -1.2643267 ] } }
+]
+}
